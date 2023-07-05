fb5e2c3a9fb7e999d2a2a32e1f6756cf189634f0
Pedro Saraiva <125305226+Pedream27@users.noreply.github.com>
2023-07-04 22:40:46 -0300

    1.0.3

--- DesafioAluraBeckend01/src/br/com/saraiva/desafio/alura/cambio/view/Start.java
+++ DesafioAluraBeckend01/src/br/com/saraiva/desafio/alura/cambio/view/Start.java
@@ -4,9 +4,13 @@ import java.awt.Frame;
 
 import javax.swing.JOptionPane;
 
+import br.com.saraiva.desafio.alura.cambio.controle.Selecionador;
+
 public class Start {
 
 	public static void main(String[] args) {
+		
+		
 
 		Selecionador inicio = new Selecionador();
 
